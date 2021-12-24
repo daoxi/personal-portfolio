a0bb7274b08cac3ec3a7d47c09d5a231151350d4
Daoxi Sun <daoxisun@gmail.com>
2021-12-23 22:16:01 -0500

    removed tags testing component and reformatted

--- pages/components/ProjCard.js
+++ pages/components/ProjCard.js
@@ -5,20 +5,18 @@ function ProjCard(props) {
     <div className={classes.card}>
       <img className={classes.card_img} src={props.img} />
       <h2 className={classes.card_title}>{props.title}</h2>
-      
-      <ul>
-        {props.tags}
-      </ul>
 
       <ul className={classes.tags}>
         {/* Render each tag in the array as an li item */}
-        {
-          props.tags.map((tag,i)=>{
-            return <li key={i} className={classes.tag}>{tag}</li>
-          })
-        }
+        {props.tags.map((tag, i) => {
+          return (
+            <li key={i} className={classes.tag}>
+              {tag}
+            </li>
+          );
+        })}
       </ul>
-      
+
       <p className={classes.card_description}>{props.description}</p>
       <button className={classes.card_btn}>{props.buttontext}</button>
     </div>
