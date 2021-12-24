2183cfc5b9a11f8c6cfc7b264d3d15adb019a3ea
Daoxi Sun <daoxisun@gmail.com>
2021-12-23 22:07:04 -0500

    Added tags for tech stacks for project cards

--- pages/components/ProjCardsContainer.js
+++ pages/components/ProjCardsContainer.js
@@ -5,24 +5,28 @@ function ProjCardsContainer() {
   return (
     <div className={classes.card_grid}>
       <ProjCard
+        tags={['tag1','tag2','tag3']}
         img="assets/test.png"
         title="The Title"
         description="The Description"
         buttontext="Learn More"
       />
       <ProjCard
+        tags={['tag1']}
         img="assets/test.png"
         title="The Title 2"
         description="The Description 2"
         buttontext="Learn More"
       />
       <ProjCard
+        tags={['tag4','tag5']}
         img="assets/test.png"
         title="The Title 3"
         description="The Description 3"
         buttontext="Learn More"
       />
       <ProjCard
+        tags={['tag44','tag55','tag66']}
         img="assets/test.png"
         title="The Title 4"
         description="The Description 4"
