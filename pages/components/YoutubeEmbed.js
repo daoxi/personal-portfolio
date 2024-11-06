d7d1574df4a0882568c71ce31476bb56aba88806
Daoxi Sun <daoxisun@gmail.com>
2024-11-05 23:30:57 -0500

    improved the embedded Youtube component

--- pages/components/YoutubeEmbed.js
+++ pages/components/YoutubeEmbed.js
@@ -1,23 +1,26 @@
-/* This component is used for embedding a Youtube video, it uses syntax <YoutubeEmbed embedId="xxxxxxxxxxx"/> in JSX */
+/* This component is used for embedding a Youtube video, syntax is <YoutubeEmbed embedId="xxxxxxxxxxx"/> when being used in other components */
 import PropTypes from "prop-types";
 import classes from "./YoutubeEmbed.module.css";
 
 const YoutubeEmbed = ({ embedId }) => (
-  <div className= {classes.videoresponsive} >
-    <iframe
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-  </div>
+	<>
+		<div className={classes.videoresponsive}>
+			<iframe
+				credentialless="true" //making TrustedHTML assignment no longer required (both TrustedHTML assignment and credentialless attribute are only supported on Chromium-based browsers as of 2024Nov5)
+				width="1920"
+				height="1080"
+				src={`https://www.youtube.com/embed/${embedId}`}
+				//frameBorder="0" //deprecated
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowFullScreen
+				title="embedded Youtube video"
+			/>
+		</div>
+	</>
 );
 
 YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired
+	embedId: PropTypes.string.isRequired,
 };
 
 export default YoutubeEmbed;
